64365b945b0967b19c7a923085e0ce933247656e
nummy <allen_li89@163.com>
2016-09-27 15:19:06 +0800

    Update jquery.timeSelect.js

--- jquery.timeSelect.js
+++ jquery.timeSelect.js
@@ -5,18 +5,21 @@
         this.weeks = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
         this.init();
     }
-
     timeSelect.prototype = {
+        // init plugin
         init: function() {
             this.initTable();
         },
+        // bind events
         bindEvent: function() {
             var that = this;
-            $("input[class^=x_]").change(function() {
+            var $table = this.$elem;
+            // click x axis
+            $table.find("input[class^=x_]").change(function() {
                 var xy = that.getXy($(this).attr("class"));
                 var cki = this;
 
-                $("tr:gt(1)").each(function(index) {
+                $table.find("tr:gt(1)").each(function() {
                     var t = $(this).find("td:eq(" + (parseInt(xy.x) + 1) + ")>div");
                     if (cki.checked) {
                         if (!t.hasClass("checked")) {
@@ -29,28 +32,28 @@
                 if (cki.checked) {
                     for (var i = 1; i <= 7; i++) {
                         if (that.isRowAllChecked(i)) {
-                            $("input[class=y_0_" + i + "]").prop("checked", true);
+                            $table.find("input[class=y_0_" + i + "]").prop("checked", true);
                         }
                     }
 
                 } else {
-                    $("input[class^=y_]").each(function(i) {
-                        var yckd = $("tr:eq(" + (i + 1) + ")>td").children("div.checked");
+                    $table.find("input[class^=y_]").each(function(i) {
+                        var yckd = $table.find("tr:eq(" + (i + 1) + ")>td").children("div.checked");
                         if (yckd.length == 48) {
-                            $(".y_0_" + (i + 1)).prop("checked", true);
+                            $table.find(".y_0_" + (i + 1)).prop("checked", true);
                         } else {
-                            $(".y_0_" + (i + 1)).prop("checked", false);
+                            $table.find(".y_0_" + (i + 1)).prop("checked", false);
                         }
                     });
                 }
                 that.displayInfo();
             });
-
-            $("input[class^=y_]").change(function(index) {
+            // click y axis
+            $table.find("input[class^=y_]").change(function() {
                 var xy = that.getXy($(this).attr("class"));
                 var cki = this;
 
-                $("tr:eq(" + (parseInt(xy.y) + 1) + ")>td").each(function(index) {
+                $table.find("tr:eq(" + (parseInt(xy.y) + 1) + ")>td").each(function() {
                     var t = $(this).children("div");
                     if (cki.checked) {
                         if (!t.hasClass("checked")) {
@@ -63,29 +66,27 @@
                 if (cki.checked) {
                     for (var i = 1; i <= 48; i++) {
                         if (that.isColumnAllChecked(i)) {
-                            $("input[class=x_" + i + "_0" + "]").prop("checked", true);
+                            $table.find("input[class=x_" + i + "_0" + "]").prop("checked", true);
                         }
                     }
 
                 } else {
-                    $("input[class^=x_]").each(function(i) {
+                    $table.find("input[class^=x_]").each(function() {
                         var xy = that.getXy($(this).attr("class"));
-                        var xckd = $("div[class^='span xy_" + xy.x + "']").filter(".checked");
+                        var xckd = $table.find("div[class^='span xy_" + xy.x + "']").filter(".checked");
                         if (xckd.length == 7) {
-                            $(".x_" + xy.x + "_0").prop("checked", true);
+                            $table.find(".x_" + xy.x + "_0").prop("checked", true);
                         } else {
-                            $(".x_" + xy.x + "_0").prop("checked", false);
+                            $table.find(".x_" + xy.x + "_0").prop("checked", false);
                         }
                     });
                 }
                 that.displayInfo();
             });
-
-            $("div.span").click(function(index) {
+            // click div span
+            $table.find("div.span").click(function() {
                 var xy = that.getXy($(this).attr("class").replace(" checked", ""));
-                var cki = this;
                 var cked = $(this).hasClass("checked");
-
                 if (cked) {
                     $(this).removeClass("checked");
                 } else {
@@ -94,11 +95,13 @@
                 that.cacheCheck(xy);
                 that.displayInfo();
             });
-
-            $("button.clearButton").click(function() {
+            // click reset button
+            $table.find(".clearButton").click(function(e) {
                 that.clear();
+                e.preventDefault();
             });
         },
+        // init table
         initTable: function() {
             var $table = this.$elem;
             var $header = this.initHeader();
@@ -109,11 +112,13 @@
             $table.append($footer);
             this.bindEvent();
         },
+        // init table header
         initHeader: function() {
-            var $header = []
-            var $tr = $("<tr>");
-            var $td;
-            for (var i = 0; i < 25; i++) {
+            var $header = [],
+              $tr = $("<tr>"),
+              $td,
+              i;
+            for (i = 0; i < 25; i++) {
                 if (i == 0) {
                     $td = $("<td colspan=2 rowspan=2></td>");
                 } else {
@@ -123,7 +128,7 @@
             }
             $header.push($tr);
             $tr = $("<tr>");
-            for (var i = 0; i < 50; i++) {
+            for (i = 0; i < 50; i++) {
                 if (i < 2) {
                     continue;
                 } else {
@@ -134,6 +139,7 @@
             $header.push($tr);
             return $header;
         },
+        // init table body
         initBody: function() {
             var $tr, $td;
             var body = [];
@@ -153,63 +159,57 @@
             }
             return body;
         },
+        // init table footer
         initFooter: function() {
             var $tr = $("<tr>");
             var $labelTd = $("<td colspan=2><p>已选时间</p></td>");
-            var $infoTd = $("<td class='info'colspan=46></td>");
-            var $clearTd = $("<td class='clear' colspan=2><p><button class='clearButton'>重置</button></p></td>");
+            var $infoTd = $("<td class='info' colspan=46></td>");
+            var $clearTd = $("<td class='clearTd' colspan=2><p><a class='clearButton'>重置</a></p></td>");
             $tr.append($labelTd);
             $tr.append($infoTd);
             $tr.append($clearTd);
             return $tr;
         },
+        // check if all checkbox on one row are checked
         isRowAllChecked: function(y) {
-            var yckd = $("tr:eq(" + (y + 1) + ")>td").children("div.checked");
-            if (yckd.length == 48) {
-                return true;
-            } else {
-                return false;
-            }
-
+            var yckd = this.$elem.find("tr:eq(" + (y + 1) + ")>td").children("div.checked");
+            return yckd.length == 48;
         },
+        // check if all checkbox on one column are checked
         isColumnAllChecked: function(x) {
-            var xckd = $("div[class^='span xy_" + x + "_']").filter(".checked");
-            if (xckd.length == 7) {
-                return true;
-            } else {
-                return false;
-            }
+            var xckd = this.$elem.find("div[class^='span xy_" + x + "_']").filter(".checked");
+            return xckd.length == 7;
         },
         checkRowAndColumn: function() {
             for (var i = 0; i < 7; i++) {
                 if (this.isRowAllChecked(i + 1)) {
-                    $("input[class=y_0_" + (i+1) + "]").prop("checked", true);
+                    this.$elem.find("input[class=y_0_" + (i+1) + "]").prop("checked", true);
                 }
             }
             for (var j = 0; j < 48; j++) {
                 if (this.isColumnAllChecked(j + 1)) {
-                    $("input[class=x_" + (j+1) + "_0" + "]").prop("checked", true);
+                    this.$elem.find("input[class=x_" + (j+1) + "_0" + "]").prop("checked", true);
                 }
             }
         },
         cacheCheck: function(xy) {
-            var xckd = $("div[class^='span xy_" + xy.x + "_']").filter(".checked");
+            var xckd = this.$elem.find("div[class^='span xy_" + xy.x + "_']").filter(".checked");
             if (xckd.length == 7) {
-                $(".x_" + xy.x + "_0").prop("checked", true);
+                this.$elem.find(".x_" + xy.x + "_0").prop("checked", true);
             } else {
-                $(".x_" + xy.x + "_0").prop("checked", false);
+                this.$elem.find(".x_" + xy.x + "_0").prop("checked", false);
             }
-            var yckd = $("tr:eq(" + (parseInt(xy.y) + 1) + ")>td").children("div.checked");
+            var yckd = this.$elem.find("tr:eq(" + (parseInt(xy.y) + 1) + ")>td").children("div.checked");
             if (yckd.length == 48) {
-                $(".y_0_" + xy.y).prop("checked", true);
+                this.$elem.find(".y_0_" + xy.y).prop("checked", true);
             } else {
-                $(".y_0_" + xy.y).prop("checked", false);
+                this.$elem.find(".y_0_" + xy.y).prop("checked", false);
             }
         },
         initArray: function() {
-            var arr = new Array();
+            var arr = [];
             for (var i = 0; i < 7; i++) {
-                arr[i] = new Array();
+                arr[i] = [];
                 for (var j = 0; j < 48; j++) {
                     arr[i][j] = 0;
                 }
@@ -258,14 +258,13 @@
                             break;
                         }
                     }
-
                     result.push(this.minutesToHour(start * 30 - 30) + "-" + this.minutesToHour(end * 30));
                 }
             }
             return result;
         },
         displayInfo: function() {
-            var checkeds = $("div.checked");
+            var checkeds = this.$elem.find("div.checked");
             var that = this;
             if (checkeds.length > 0) {
                 var data = {};
@@ -282,32 +281,28 @@
                 $.each(data, function(k, v) {
                     timeStr.push("</p>" + k + ":" + that.getTimeSection(v).join(",") + "</p>");
                 });
-                $("td.info").html(timeStr.join(""));
-            }else{
-                $("td.info").html("");
+                this.$elem.find("td.info").html(timeStr.join(""));
             }
         },
         minutesToHour: function(minutes) {
-            var result = "";
             var hour = Math.floor(minutes / 60);
-            var minutes = minutes % 60;
+            minutes = minutes % 60;
             if (minutes == 0) {
                 minutes = "00";
             }
-            result = hour + ":" + minutes;
-            return result;
+            return hour + ":" + minutes;
         },
         clear: function() {
-            $("div.checked").each(function() {
+            this.$elem.find("div.checked").each(function() {
                 $(this).removeClass("checked");
             });
-            $("input[class^=y_]").each(function(i) {
+            this.$elem.find("input[class^=y_]").each(function(i) {
                 $(this).prop("checked", false);
             });
-            $("input[class^=x_]").each(function(i) {
+            this.$elem.find("input[class^=x_]").each(function(i) {
                 $(this).prop("checked", false);
             });
-            $("td.info").html("");
+            this.$elem.find("td.info").html("");
 
         },
         getXy: function(cls) {
@@ -323,7 +318,7 @@
             for (var i = 0; i < arr.length; i++) {
                 for (var j = 0; j < arr[i].length; j++) {
                     if(arr[i][j] == 1){
-                         $("div.xy_" + (j + 1) + "_" + (i + 1)).addClass("checked");
+                         this.$elem.find("div.xy_" + (j + 1) + "_" + (i + 1)).addClass("checked");
                     }
                 }
             }
@@ -332,7 +327,7 @@
         },
         getValue: function() {
             var that = this;
-            var checkeds = $("div.checked");
+            var checkeds = this.$elem.find("div.checked");
             var result = "";
             if (checkeds.length > 0) {
                 var arr = that.initArray();
@@ -344,13 +339,14 @@
                 });
                 result = that.arrayToString(arr);
             }
+            window.times = result;
             return result;
         },
         destroy: function(){
             this.$elem.empty();
             this.$elem.removeData("timeSelect");
         }
-    }
+    };
 
     $.fn.timeSelect = function(options) {
         var settings = $.extend({}, $.fn.timeSelect.defaults, typeof options === "object" ? options : {});
